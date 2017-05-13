3a2d091c0520535249d05026156439f315a0c4fd
minhlinh <minhlinh100@gmail.com>
2017-05-13 08:26:19 +0700

    Update all_d.js

--- all_d.js
+++ all_d.js
@@ -9,6 +9,19 @@ document.writeln("<style>#LRfloater2{max-height: 100%;}</style>");
 if(!window.jQuery) {
 document.writeln("<script language=\"javascript\" src=\"http://chuyende.phongkhamngoquyen.com/js_all/jquery-1.11.3.min.js\"></script>");
 }
+jQuery(document).ready(function($){
+	setTimeout(function(){
+		if($("#LRdiv2").length<1)
+		{
+			console.log('Add subiz');
+window._sbzq||function(e){e._sbzq=[];var t=e._sbzq;t.push(["_setAccount",64439]);var n=e.location.protocol=="https:"?"https:":"http:";var r=document.createElement("script");r.type="text/javascript";r.async=true;r.src=n+"//static.subiz.com/public/js/loader.js";var i=document.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)}(window);
+		}
+		else{
+			console.log('Isset live');
+		}
+
+	},20000);
+});
 document.writeln("<script language=\"javascript\" src=\"http://chuyende.phongkhamngoquyen.com/js_all/all_form.js\"></script>");
 document.writeln("<script language=\"javascript\" src=\"http://chuyende.phongkhamngoquyen.com/js_all/all_formcall.js\"></script>");
 /* */
