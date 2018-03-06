4a9a98de5526ee80fcc10f280913ca6845a5e490
minhlinh <minhlinh100@gmail.com>
2018-03-06 11:56:43 +0700

    Update all_m.js

--- all_m.js
+++ all_m.js
@@ -290,25 +290,28 @@ $("#tv1017 .canel").click(function(){
 });
 
 function show_tv1017(){
-	
-	if($("#tv1017").data('time') == 2)
+	if($("#tv1017").data('time')<4)
 	{
-		var time = 15000;
+		if($("#tv1017").data('time') == 2)
+		{
+			var time = 15000;
+		}
+		else
+		{
+			var time = 30000;
+		}
+		console.log('tv1017 ' + time);    	
+		setTimeout(function(){
+				if($("#LRMINIBar").css("display")=='block')
+				{
+					$("#tv1017").data('time',3);
+					$("#tv1017").show('slow');
+				}
+				else{
+					show_tv1017();
+				}
+		},time);
 	}
-	else
-	{
-		var time = 30000;
-	}
-	console.log('tv1017 ' + time);    	
-	setTimeout(function(){
-			if($("#LRMINIBar").css("display")=='block')
-			{
-				$("#tv1017").show('slow');
-			}
-			else{
-				show_tv1017();
-			}
-	},time);
 }
 
 });
