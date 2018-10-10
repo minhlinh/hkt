1eec7c056039e256dbf90370cc8449806c26f2bb
minhlinh <minhlinh100@gmail.com>
2018-10-10 11:50:35 +0700

    Update all_d.js

--- all_d.js
+++ all_d.js
@@ -273,7 +273,7 @@ document.writeln("<style>@import url(\"http://fonts.googleapis.com/css?family=Mo
     });
 
 
-//popup mobile
+/*/popup mobile
     var tv1017 = '<div id="tv1017" data-time="1" style="background:url(\'http://lib.phongkhamngoquyen.com/m1017.png\') no-repeat center;width:281px;height:85px;display:none;margin:0 auto;position: fixed;z-index:99999999999;top:45%;left:0;width:100%">'
                 +   '<a style="width:50%;display:inline-block;height:85px" href="javascript:void(0)" class="canel"></a>'
                 +   '<a style="width:50%;display:inline-block;height:85px" href="javascript:void(0)" onclick="openZoosUrl();LR_HideInvite();return false;" title="Tư vấn online" target="_blank"></a>'
@@ -345,8 +345,11 @@ function show_tv1017(){
 		},time);
 	}
 }
+
 //Banner 3.2.2018
 // if ($(window).width() < 500) {
 // $( "body" ).prepend('<a href="javascript:void(0)" onclick="openZoosUrl();LR_HideInvite();return false;" title="Tư vấn online" target="_blank"><img src="http://chuyende.phongkhamngoquyen.com/slide/_banner/2018-01/m'+window.location.hostname+'.png" alt="img"></a>');
 // }
 });
+
+*/
